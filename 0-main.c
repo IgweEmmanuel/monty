eb6f9ced46e89e5fd087c88c460a1691e517e86f
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-01-19 00:16:36 +0000

    monty

--- 0-main.c
+++ 0-main.c
@@ -6,11 +6,12 @@ channel_t channel = {NULL, NULL, NULL, 0};
 * @argv: monty file location
 * Return: 0 on success
 */
-
 int main(int argc, char *argv[])
 {
-	char buffer[BUFFER_SIZE];
+	char *content;
 	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
@@ -19,42 +20,27 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
 	channel.file = file;
-
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	while (fgets(buffer, sizeof(buffer), file) != NULL)
+	while (read_line > 0)
 	{
-		size_t read_line = strlen(buffer);
-		char *content = malloc(read_line + 1);
-
-		if (content == NULL)
-		{
-			perror("Error allocating memory for line");
-			exit(EXIT_FAILURE);
-		}
-
-		strcpy(content, buffer);
+		content = NULL;
+		read_line = getline(&content, &size, file);
 		channel.content = content;
 		counter++;
-
+		
 		if (read_line > 0)
 		{
 			execute_ic(content, &stack, counter, file);
 		}
-
 		free(content);
 	}
-
 	free_stack_ic(stack);
 	fclose(file);
-
-	return (0);
+return (0);
 }
-
